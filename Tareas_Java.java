7243d5087b52bd9426247470c4c47edf3e1aa26f
isenriquez <65500311+isenriquez@users.noreply.github.com>
2020-05-29 22:29:45 -0500

    Caracteristicas_GitHub

---
+++ Tareas_Java.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareas_java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Israel Enriquez
+ */
+import java.util.Scanner;
+public class Tareas_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner objeto=new Scanner(System.in);//new rerva espacio de memoria
+        System.out.println("UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+
+        System.out.println("¿Que es GitHub?");
+        System.out.println("Sitio web que presta servicio de almacenamiento en la nube, manejo y control\n" +
+"de codigos, en el cual puede llevar un registro de cualquier cambio o \n" +
+"modificación sobre este codigo.");
+
+        System.out.println("¿Cuales son sus funciones?");
+        System.out.println("-Almacenaje  \n" +
+"-Registro    \n" +
+"-Control y colaboración   \n" +
+"De codigos o tambien de Libros");
+
+        System.out.println("Ingrese la caracteristicas del GitHub:");
+        System.out.println("Característica 1:");
+        String caracteristica1=objeto.nextLine();
+
+        System.out.println("Característica 2:");
+        String caracteristica2=objeto.nextLine();
+
+        System.out.println("Caracteristica 3:");
+        String caracteristica3=objeto.nextLine();
+
+        System.out.println("Característica 4:");
+        String caracteristica4=objeto.nextLine();
+
+        System.out.println("Característica 5:");
+        String caracteristica5=objeto.nextLine();
+
+        System.out.println("Gracias por utilizar el programa");
+        /*
+        System.out.println("Ingrese 'n' si tiene otra característica que añadir,si no tiene nada mas que añadir pulse cualquier tecla");
+
+        String opcion=objeto.nextLine();
+
+         if( opcion= ) {
+              System.out.println("Ingrese caracteristica 6:");
+               String caracteristica6=objeto.nextLine();
+               System.out.println("Gracias por utilizar el programa");
+          }
+          else   {
+               System.out.println("Gracias por utilizar el programa");
+          }
+    */
+
+
+
+
+
+    }
+
+}
